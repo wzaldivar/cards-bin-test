26e6116c47479ecc7507a8c539013c42320c75bd
Walber Zaldivar Herrera <walber.zaldivar@gmail.com>
2024-03-20 10:44:10 -0300

    initial commit

---
+++ src/main/java/group/etraveli/card/cost/CardCostApiApplication.java
@@ -0,0 +1,13 @@
+package group.etraveli.card.cost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardCostApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardCostApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/group/etraveli/card/cost/CardCostApiApplicationTests.java
@@ -0,0 +1,13 @@
+package group.etraveli.card.cost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardCostApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
